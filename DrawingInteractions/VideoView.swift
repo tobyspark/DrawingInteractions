98c4c990132ee53dbc7b3180ab8d280bc71db6bf
Toby Harris <operator@tobyz.net>
2018-03-23 13:33:34 +0000

    Displays test video

---
+++ DrawingInteractions/VideoView.swift
@@ -0,0 +1,32 @@
+//
+//  VideoView.swift
+//  DrawingInteractions
+//
+//  Created by Toby Harris on 23/03/2018.
+//  Copyright © 2018 Toby Harris. All rights reserved.
+//
+
+import UIKit
+import AVFoundation
+
+class VideoView: UIView {
+
+    var player: AVPlayer? {
+        get {
+            return playerLayer.player
+        }
+        set {
+            playerLayer.player = newValue
+        }
+    }
+    
+    var playerLayer: AVPlayerLayer {
+        return layer as! AVPlayerLayer
+    }
+    
+    // Override UIView property
+    override static var layerClass: AnyClass {
+        return AVPlayerLayer.self
+    }
+
+}
--- DrawingInteractions/ViewController.swift
+++ DrawingInteractions/ViewController.swift
@@ -7,12 +7,25 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
+    
+    // MARK: Properties
+    
+    var videoView: VideoView {
+        return view as! VideoView
+    }
+
+    // MARK: View Life Cycle
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        guard let moviePath = Bundle.main.url(forResource: "testvideo", withExtension: "mov") else {
+            fatalError("Can't find testvideo")
+        }
+        videoView.player = AVPlayer.init(url: moviePath)
     }
 
     override func didReceiveMemoryWarning() {
