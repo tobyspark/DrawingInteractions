d6b520fc97c7e3d6b435865a1155a6e3133f16d9
Toby Harris <operator@tobyz.net>
2018-03-26 12:00:00 +0100

    Stylus draws, fingers do UI.

--- DrawingInteractions/ViewController.swift
+++ DrawingInteractions/ViewController.swift
@@ -14,14 +14,69 @@ class ViewController: UIViewController {
     // MARK: Properties
     
     var timelineView: VideoTimelineView!
+    var canvasView: CanvasView!
     var videoView: VideoView {
         return view as! VideoView
     }
     
     let stripHeight:CGFloat = 44
     
-    override func viewDidLayoutSubviews() {
-        timelineView.boundsDidChange()
+    // MARK: Touch Handling
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let stylusTouches = touches.filter { return $0.type == .stylus }
+        if !stylusTouches.isEmpty {
+            canvasView.drawTouches(touches: stylusTouches, withEvent: event)
+        }
+        let otherTouches = touches.subtracting(stylusTouches)
+        if !otherTouches.isEmpty {
+            super.touchesBegan(touches, with: event)
+        }
+    }
+    
+    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let stylusTouches = touches.filter { return $0.type == .stylus }
+        if !stylusTouches.isEmpty {
+            canvasView.drawTouches(touches: stylusTouches, withEvent: event)
+        }
+        let otherTouches = touches.subtracting(stylusTouches)
+        if !otherTouches.isEmpty {
+            super.touchesBegan(touches, with: event)
+        }
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let stylusTouches = touches.filter { return $0.type == .stylus }
+        if !stylusTouches.isEmpty {
+            canvasView.drawTouches(touches: stylusTouches, withEvent: event)
+            canvasView.endTouches(touches: stylusTouches, cancel: false)
+        }
+        let otherTouches = touches.subtracting(stylusTouches)
+        if !otherTouches.isEmpty {
+            super.touchesBegan(touches, with: event)
+        }
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let stylusTouches = touches.filter { return $0.type == .stylus }
+        if !stylusTouches.isEmpty {
+            canvasView.endTouches(touches: stylusTouches, cancel: false)
+        }
+        let otherTouches = touches.subtracting(stylusTouches)
+        if !otherTouches.isEmpty {
+            super.touchesBegan(touches, with: event)
+        }
+    }
+    
+    override func touchesEstimatedPropertiesUpdated(_ touches: Set<UITouch>) {
+        let stylusTouches = touches.filter { return $0.type == .stylus }
+        if !stylusTouches.isEmpty {
+            canvasView.updateEstimatedPropertiesForTouches(touches: stylusTouches)
+        }
+        let otherTouches = touches.subtracting(stylusTouches)
+        if !otherTouches.isEmpty {
+            super.touchesEstimatedPropertiesUpdated(touches)
+        }
     }
 
     // MARK: View Life Cycle
@@ -33,11 +88,18 @@ class ViewController: UIViewController {
             fatalError("Can't find testvideo")
         }
         
+        canvasView = CanvasView()
+        canvasView.frame = view.frame
+        canvasView.translatesAutoresizingMaskIntoConstraints = true
+        canvasView.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        canvasView.backgroundColor = .clear
+        view.addSubview(canvasView)
+        
         let strip = CGRect(x: 0, y: 0, width: view.frame.width, height: stripHeight)
         timelineView = VideoTimelineView(frame: strip)
         timelineView.translatesAutoresizingMaskIntoConstraints = true
         timelineView.center = CGPoint(x: view.bounds.midX, y: view.bounds.midY)
-        timelineView.autoresizingMask = [UIViewAutoresizing.flexibleWidth, UIViewAutoresizing.flexibleTopMargin, UIViewAutoresizing.flexibleBottomMargin]
+        timelineView.autoresizingMask = [.flexibleWidth, .flexibleTopMargin, .flexibleBottomMargin]
         timelineView.delegate = videoView
         videoView.delegate = timelineView
         
@@ -50,5 +112,9 @@ class ViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    override func viewDidLayoutSubviews() {
+        timelineView.boundsDidChange()
     }
 }
